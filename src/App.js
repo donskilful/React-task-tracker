e9dfb3bef5d5f9264132ddb0f87e7692e184465a
donskilful <donskilful@gmail.com>
2021-09-08 21:52:41 +0100

    React task tracker completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -46,8 +47,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      { showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No task available to show'}
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('cliked');
-  }
-
+const Header = ({ onAdd, title, showAdd }) => {
 
   return (
     <div>
       <header className='header'>
         <h1>{ title }</h1>
-        <Button color='green' text='Add' onClick={onClick} />
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
       </header>
     </div>
   )
